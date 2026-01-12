3a70dec7920e7e3f3ddcda8f625600f0c187c38c
Hacrrr.dev <freakgaming2009@gmail.com>
2026-01-12 21:43:28 +0530

    Add files via upload

---
+++ themes/Saturn/vite.config.js
@@ -0,0 +1,35 @@
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import path from "path";
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                "themes/Saturn/css/app.css",
+                "themes/Saturn/js/app.js"
+            ],
+            buildDirectory: "Saturn/",
+        }),
+        {
+            name: "blade",
+            handleHotUpdate({ file, server }) {
+                if (file.endsWith(".blade.php")) {
+                    server.ws.send({
+                        type: "full-reload",
+                        path: "*",
+                    });
+                }
+            },
+        },
+    ],
+    css: {
+        postcss: {
+            plugins: [
+                require("tailwindcss")({
+                    config: path.resolve(__dirname, "tailwind.config.js"),
+                }),
+            ],
+        },
+    },
+});
